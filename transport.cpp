c74bb222bfb51429818cdc44d32214e69d99fdec
Declan <declanpnell@gmail.com>
2022-05-20 17:53:27 +0100

    missing factor of 2 in coupling matrices

--- transport.cpp
+++ transport.cpp
@@ -12,6 +12,8 @@ void get_transmission(const Parameters &parameters, const std::vector<double> &k
     const std::vector<std::vector<EmbeddingSelfEnergy>> &leads, std::vector<dcomp> &transmission_up, std::vector<dcomp> &transmission_down, const int voltage_step){
 
     double num_k_points = parameters.chain_length_x * parameters.chain_length_y;
+    std::cout << num_k_points << std::endl;
+
     for(int kx_i = 0; kx_i < parameters.chain_length_x; kx_i++) {
         for(int ky_i = 0; ky_i < parameters.chain_length_y; ky_i++) {
             Interacting_GF gf_interacting_up(parameters,
@@ -25,8 +27,8 @@ void get_transmission(const Parameters &parameters, const std::vector<double> &k
                 leads.at(kx_i).at(ky_i).self_energy_right, voltage_step);             
 
             for(int r = 0; r < parameters.steps; r++){
-                dcomp coupling_left = leads.at(kx_i).at(ky_i).self_energy_left.at(r).imag();
-                dcomp coupling_right = leads.at(kx_i).at(ky_i).self_energy_right.at(r).imag();
+                dcomp coupling_left = 2 * leads.at(kx_i).at(ky_i).self_energy_left.at(r).imag();
+                dcomp coupling_right = 2* leads.at(kx_i).at(ky_i).self_energy_right.at(r).imag();
                 transmission_up.at(r) += 1.0 / num_k_points * (coupling_left * gf_interacting_up.interacting_gf.at(r)(0, parameters.chain_length - 1) *
                     coupling_right * std::conj(gf_interacting_up.interacting_gf.at(r)(0, parameters.chain_length - 1)));
                 transmission_down.at(r) += 1.0 / num_k_points * (coupling_left * gf_interacting_down.interacting_gf.at(r)(0, parameters.chain_length - 1) *
